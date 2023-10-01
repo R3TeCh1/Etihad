da7d1fdafd00018b214e220947341cbd74895d62
David Rischow <drischow@stud.hs-heilbronn.de>
2023-10-01 18:41:19 +0200

    Add Content to Seat

--- etihad23/src/etihad/Seat.java
+++ etihad23/src/etihad/Seat.java
@@ -4,4 +4,23 @@ package etihad;
  * @version 30.09.2023
  */
 public class Seat {
+    String location;
+
+    boolean reservation;
+
+    Plane plane;
+
+    public Seat(String location, Plane plane) {
+        this.location = location;
+        this.plane = plane;
+    }
+
+    public String reserve() {
+        if (reservation) {
+            return "Seat is already reserved by someone";
+        } else {
+            reservation = true;
+            return "Seat reserved";
+        }
+    }
 }
