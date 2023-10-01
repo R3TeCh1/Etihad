f35b2c3760d15df264983990dffd8c7856cb704c
R3TeCh_ <dadas.99@hotmail.de>
2023-10-01 16:55:34 +0200

    Flight JavaDoc wurde hinzugefügt

--- etihad23/src/etihad/Flight.java
+++ etihad23/src/etihad/Flight.java
@@ -6,8 +6,11 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
+ * Die Klasse Flight repräsentiert einen Flug mit verschiedenen Eigenschaften wie Datum, Flugnummer,
+ * Herkunfts- und Zielorte, Airline, Passagierliste, Piloten und Flugzeug.
+ *
  * @author Kadir Erzurum
- * @version 30.09.2023
+ * @version 01.10.2023
  */
 public class Flight {
 
@@ -33,7 +36,20 @@ public class Flight {
   private Plane vehicle;
 
 
-  //Konstruktor
+  /**
+   * Konstruktor für die Klasse Flight.
+   *
+   * @param date Das Datum des Fluges.
+   * @param flightNum Die Flugnummer.
+   * @param origin Die Flughäfen des Ursprungs.
+   * @param destination Die Flughäfen des Ziels.
+   * @param organizer Die Fluggesellschaft (Airline).
+   * @param passengers Die Liste der Passagiere.
+   * @param captain Der Kapitän des Fluges.
+   * @param coPilot Der Co-Pilot des Fluges.
+   * @param flightEngineer Der Flugingenieur des Fluges.
+   * @param vehicle Das Flugzeug, das für den Flug verwendet wird.
+   */
   public Flight(LocalDate date, String flightNum, Airport [] origin, Airport [] destination, Airline organizer, List<Passenger> passengers, Pilot captain, Pilot coPilot, Pilot flightEngineer, Plane vehicle){
     this.date = date;
     this.flightNum = flightNum;
@@ -50,32 +66,50 @@ public class Flight {
   }
 
   //METHODEN----------------------------------------------------------------
-  //+refuel
+
+  /**
+   * Betankt das Flugzeug.
+   */
   public void refuel(){
     System.out.println(toString() + " wurde betankt.");
   }
 
-  //+takeOff
+  /**
+   * Lässt das Flugzeug abheben.
+   */
   public void takeOff(){
     System.out.println(toString() + " ist abgehoben");
   }
 
-  //+land
+  /**
+   * Lässt das Flugzeug landen.
+   */
   public void land(){
     System.out.println(toString() + " ist gelandet.");
   }
 
-  //+delay
+  /**
+   * Verzögert den Flug auf das angegebene Datum.
+   *
+   * @param delayedTo Das neue Datum für die Verspätung.
+   */
   public void delay(LocalDate delayedTo){
     setDate(delayedTo);
     System.out.println(toString() + " verspätet sich - Neue Zeit: " + delayedTo);
   }
 
-  //+cancel
+  /**
+   * Storniert den Flug.
+   */
   public void cancel(){
-    System.out.println("Der Flug " + toString() + flightNum + " wurde storniert.");
+    System.out.println("Der Flug " + toString() + " " + flightNum + " wurde storniert.");
   }
 
+  /**
+   * Fügt einen Passagier zur Passagierliste hinzu.
+   *
+   * @param passenger Der hinzuzufügende Passagier.
+   */
   public void addPassenger(Passenger passenger) {
     if (passengers.size() < MAX_PASSENGERS) {
       passengers.add(passenger);
@@ -85,30 +119,56 @@ public class Flight {
     }
   }
 
+  /**
+   * Weist dem Flug einen Kapitän zu.
+   *
+   * @param captain Der zuzuweisende Kapitän.
+   */
   public void assignCaptain(Pilot captain) {
     this.captain = captain;
     System.out.println(toString() + " wurde zum Kapitän " + captain.getName());
   }
 
+  /**
+   * Weist dem Flug einen Co-Piloten zu.
+   *
+   * @param coPilot Der zuzuweisende Co-Pilot.
+   */
   public void assignCoPilot(Pilot coPilot) {
     this.coPilot = coPilot;
     System.out.println(toString() + " wurde zum Co-Pilot " + coPilot.getName());
   }
 
+  /**
+   * Weist dem Flug einen Flugingenieur zu.
+   *
+   * @param flightEngineer Der zuzuweisende Flugingenieur.
+   */
   public void assignFlightEngineer(Pilot flightEngineer) {
     this.flightEngineer = flightEngineer;
     System.out.println(toString() + " wurde zum Flugingenieur" + flightEngineer.getName());
   }
 
+  /**
+   * Weist dem Flug eine Airline zu.
+   *
+   * @param airline Die zuzuweisende Airline.
+   */
   public void assignAirline(Airline airline) {
     this.organizer = airline;
     System.out.println(toString() + " wurde zur Airline " + airline.getName());
   }
 
+  /**
+   * Startet den Flug und gibt eine Erfolgsmeldung aus.
+   */
   public void startFlight() {
     System.out.println(toString() + " hat den Flug erfolgreich gestartet.");
   }
 
+  /**
+   * Beendet den Flug und gibt eine Erfolgsmeldung aus.
+   */
   public void finishFlight() {
     System.out.println(toString() + " hat den Flug erfolgreich beendet.");
   }
@@ -199,6 +259,11 @@ public class Flight {
   }
 
   //TOSTRING--------------------------------------------------------
+  /**
+   * Gibt eine Zeichenfolge zurück, die den Klassennamen und die Flugnummer darstellt.
+   *
+   * @return Eine Zeichenfolge im Format "Klassenname Flugnummer".
+   */
   @Override
   public String toString(){
     return getClass().getSimpleName() + " " + flightNum;
